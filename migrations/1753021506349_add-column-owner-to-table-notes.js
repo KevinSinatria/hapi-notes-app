c9179425769de45ad45145fcc7f7a04c9ecd7a5e
KevinSinatria <kevinsinatria8@gmail.com>
2025-07-20 21:56:05 +0700

    authorization implemented

---
+++ migrations/1753021506349_add-column-owner-to-table-notes.js
@@ -0,0 +1,23 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+  pgm.addColumn("notes", {
+    owner: {
+      type: "VARCHAR(50)",
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+  pgm.dropColumn("notes", "owner");
+};
+
+module.exports = { up, down };
